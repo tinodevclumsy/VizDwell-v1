23f29d6bb5ee322ab1d08e0b78d65d921c119ac5
tinodevclumsy <seungjun.dev@gmail.com>
2024-09-10 21:32:04 -0700

    FEAT: installed r3f-perf to monitor performance and added environment

--- src/components/PropertyCanvas.jsx
+++ src/components/PropertyCanvas.jsx
@@ -1,24 +1,25 @@
 import { Canvas } from "@react-three/fiber";
 import PropertyModel from "./PropertyModel";
-import { PerspectiveCamera, OrbitControls } from "@react-three/drei";
+import {
+  PerspectiveCamera,
+  OrbitControls,
+  Environment,
+} from "@react-three/drei";
+import { Perf } from "r3f-perf";
 
 const Camera = () => {
-  return (
-    <PerspectiveCamera
-      makeDefault
-      fov={75}
-      position={[0, 0, 3]}
-    />
-  );
+  return <PerspectiveCamera makeDefault fov={75} position={[0, 0, 3]} />;
 };
 
 const Control = () => {
-    return <OrbitControls />
-}
+  return <OrbitControls />;
+};
 
 const PropertyCanvas = () => {
   return (
     <Canvas>
+      <Perf position="top-left" />
+      <Environment preset="sunset" background backgroundBlurriness={1} />
       <Camera />
       <Control />
       <PropertyModel />
