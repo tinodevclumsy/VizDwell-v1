8ece0d6cf67a6780c71ceeec5e4e32effc16a780
tinodevclumsy <seungjun.dev@gmail.com>
2024-09-11 17:40:17 -0700

    FEAT: implemented model

--- src/components/PropertyCanvas.jsx
+++ src/components/PropertyCanvas.jsx
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import { Canvas } from "@react-three/fiber";
 import PropertyModel from "./PropertyModel";
-import { HemisphereLightHelper } from "three";
+import { HemisphereLightHelper, DirectionalLightHelper } from "three";
 
 import {
   PerspectiveCamera,
@@ -17,16 +17,42 @@ const Camera = () => {
 };
 
 const Light = () => {
-  const lightRef = useRef();
+  const hemisphereLightRef = useRef();
+  const directionalLightRef = useRef();
 
-  useHelper(lightRef, HemisphereLightHelper, 5);
+  useHelper(hemisphereLightRef, HemisphereLightHelper, 5);
+  useHelper(directionalLightRef, DirectionalLightHelper, 5);
+
+    const directionalCtl = useControls('Directional Light', {
+    visible: true,
+    position: {
+      x: 5,
+      y: 8,
+      z: 5,
+    },
+    castShadow: true,
+  })
+
   return (
-    <hemisphereLight
-      ref={lightRef}
-      skyColor={"#fff"}
-      groundColor={"#444"}
-      intensity={3}
-    />
+    <>
+      <hemisphereLight
+        ref={hemisphereLightRef}
+        skyColor={"#fff"}
+        groundColor={"#444"}
+        intensity={1}
+      />
+      <directionalLight
+        ref={directionalLightRef}
+        visible={directionalCtl.visible}
+        position={[
+          directionalCtl.position.x,
+          directionalCtl.position.y,
+          directionalCtl.position.z,
+        ]}
+        castShadow={directionalCtl.castShadow}
+        intensity={2}
+      />
+    </>
   );
 };
 
@@ -37,11 +63,11 @@ const Control = () => {
     rotateSpeed,
     maxPolarAngle,
     minPolarAngle,
-  } = useControls({
+  } = useControls('OrbitControls', {
     enableDamping: { value: true },
     dampingFactor: { value: 0.1, min: 0, max: 1, step: 0.01 },
     rotateSpeed: { value: 1, min: 0.1, max: 5 },
-    maxPolarAngle: { value: Math.PI / 2, min: 0, max: Math.PI },
+    maxPolarAngle: { value: 1.3, min: 0, max: Math.PI },
     minPolarAngle: { value: 0, min: 0, max: Math.PI },
   });
 
@@ -58,7 +84,7 @@ const Control = () => {
 
 const PropertyCanvas = () => {
   return (
-    <Canvas>
+    <Canvas shadows gl={{ antialias: true }}>
       <Perf position="top-left" />
       <Environment preset="sunset" background backgroundBlurriness={1} />
       <Camera />
--- src/components/PropertyModel.jsx
+++ src/components/PropertyModel.jsx
@@ -1,9 +1,15 @@
 const PropertyModel = () => {
   return (
-    <mesh>
-      <boxGeometry />
-      <meshStandardMaterial color="orange" />
-    </mesh>
+    <>
+      <mesh position={[0, 0.5, 0]} castShadow receiveShadow>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} receiveShadow>
+        <planeGeometry args={[10, 10]} />
+        <meshStandardMaterial color="red" />
+      </mesh>
+    </>
   );
 };
 
