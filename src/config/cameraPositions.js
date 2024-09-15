e794d572c5f8d49cd81528ccc168b34cf033ba31
tinodevclumsy <seungjun.dev@gmail.com>
2024-09-15 16:55:14 -0700

    FEAT: added an exit button and event on click

---
+++ src/config/cameraPositions.js
@@ -0,0 +1,18 @@
+export const CAMERA_POSITIONS = {
+  DEFAULT: {
+    name: "default",
+    position: {
+      x: 16.66727556654493,
+      y: 15.624022372761212,
+      z: 11.673199871357799,
+    },
+  },
+  INTERIOR: {
+    name: "interior",
+    position: {
+      x: 5.1,
+      y: 7.4,
+      z: -0.7,
+    },
+  },
+};
