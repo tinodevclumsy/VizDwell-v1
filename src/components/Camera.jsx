560f5fb3bf3456e8837a4e78904282f636d4987d
tinodevclumsy <seungjun.dev@gmail.com>
2024-09-20 16:51:00 -0700

    FEAT: make camera movement with smooth transition

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { PerspectiveCamera } from "@react-three/drei";
 import { CAMERA_POSITIONS } from "../config/cameraPositions";
 import { useDispatch } from "react-redux";
@@ -6,77 +6,68 @@ import { useFrame } from "@react-three/fiber";
 import { toggleCameraMovement } from "../store/features/camera/cameraSlice";
 import { useControls } from "leva";
 import { useCameraView } from "../hooks/useCameraView";
+import JEASINGS from "jeasings";
 
 const Camera = ({ viewMode, isMoving }) => {
   const cameraRef = useRef();
   const dispatch = useDispatch();
   const { changeIsFrontView } = useCameraView();
-  
-  useFrame(() => {
-    console.log(cameraRef.current.position)
-    if (cameraRef.current.position.x < 5) {
-      console.log("remove view button");
-      changeIsFrontView(false);
-    } else {
-      changeIsFrontView(true);
-    }
+
+  useEffect(() => {
+    console.log("updated");
+    // update view - intial interior
     if (
       viewMode === CAMERA_POSITIONS.INTERIOR.name &&
       cameraRef.current &&
       isMoving
     ) {
-      let done = 0;
-
-      if (cameraRef.current.position.x > CAMERA_POSITIONS.INTERIOR.position.x) {
-        cameraRef.current.position.x -= 0.1;
-      } else {
-        done++;
-      }
-
-      if (cameraRef.current.position.y > CAMERA_POSITIONS.INTERIOR.position.y) {
-        cameraRef.current.position.y -= 0.1;
-      } else {
-        done++;
-      }
-
-      if (cameraRef.current.position.z < CAMERA_POSITIONS.INTERIOR.position.z) {
-        cameraRef.current.position.z += 0.1;
-      } else {
-        done++;
-      }
-
-      if (done === 3) {
-        dispatch(toggleCameraMovement());
-      }
+      new JEASINGS.JEasing(cameraRef.current.position)
+        .to(
+          {
+            x: CAMERA_POSITIONS.INTERIOR.position.x,
+            y: CAMERA_POSITIONS.INTERIOR.position.y,
+            z: CAMERA_POSITIONS.INTERIOR.position.z,
+          },
+          500
+        )
+        .easing(JEASINGS.Cubic.Out)
+        .onComplete(() => {
+          dispatch(toggleCameraMovement());
+        })
+        .start();
     }
 
+    // update view - back to default view
     if (
       viewMode === CAMERA_POSITIONS.DEFAULT.name &&
       cameraRef.current &&
       isMoving
     ) {
-      let done = 0;
-      if (cameraRef.current.position.x < CAMERA_POSITIONS.DEFAULT.position.x) {
-        cameraRef.current.position.x += 0.1;
-      } else {
-        done++;
-      }
+      new JEASINGS.JEasing(cameraRef.current.position)
+        .to(
+          {
+            x: CAMERA_POSITIONS.DEFAULT.position.x,
+            y: CAMERA_POSITIONS.DEFAULT.position.y,
+            z: CAMERA_POSITIONS.DEFAULT.position.z,
+          },
+          500
+        )
+        .easing(JEASINGS.Cubic.Out)
+        .onComplete(() => {
+          dispatch(toggleCameraMovement());
+        })
+        .start();
+    }
+  }, [viewMode, isMoving, dispatch]);
 
-      if (cameraRef.current.position.y < CAMERA_POSITIONS.DEFAULT.position.y) {
-        cameraRef.current.position.y += 0.1;
-      } else {
-        done++;
-      }
-
-      if (cameraRef.current.position.z < CAMERA_POSITIONS.DEFAULT.position.z) {
-        cameraRef.current.position.z += 0.1;
-      } else {
-        done++;
-      }
-
-      if (done === 3) {
-        dispatch(toggleCameraMovement());
-      }
+  useFrame(() => {
+    // console.log(cameraRef.current.position);
+    JEASINGS.update();
+    if (cameraRef.current.position.x < 5) {
+      console.log("remove view button");
+      changeIsFrontView(false);
+    } else {
+      changeIsFrontView(true);
     }
   });
 
