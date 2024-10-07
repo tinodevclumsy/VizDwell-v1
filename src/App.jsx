54b77df65642844a2e1ba8f92290dcab961dee57
tinodevclumsy <seungjun.dev@gmail.com>
2024-10-07 16:30:51 -0700

    CHORE: add google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import TheHeader from "./components/ui/TheHeader";
 import PropertyCanvas from "./components/PropertyCanvas";
 import "./App.css";
-import { Leva } from 'leva'
+import { Leva } from "leva";
+import ReactGA from "react-ga4";
 
 function App() {
+  ReactGA.initialize("G-EED4BSJSQY");
+
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <Leva hidden />
